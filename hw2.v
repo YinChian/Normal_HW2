15a556134e55f51ac463f797efcb9705859fefaf
YinChian <91kevin16@gmail.com>
2022-04-12 22:44:57 +0800

    完工

--- hw2.v
+++ hw2.v
@@ -2,62 +2,71 @@
 `define code_write 8'h02
 `define code_read  8'h03
 
-`define sck_counter_max = 5'h1f
+`define sck_counter_max 11'd2_000
+`define sck_counter_half 11'd1_000
 
 
 module hw2(
-	//System
+	//System0
 	input  clk_50M,
 	input  reset_n,
 	
 	//Control
 	input  write,
-	output read,
+	input  read,
 	
 	//State
-	output write_complete,
+	output reg write_complete,
 	output read_complete,
 	
 	//Data
 	input  [7:0] write_value,		//write value ---process---> DO
-	output [7:0] read_value,		//read value  <---process--- SI
+	output reg [7:0] read_value,	//read value  <---process--- SI
 											//only exsists while reading
 	
 	//Signals
 	output reg spi_csn,				//-CS--// Chip Select
 	output reg spi_sck,				//-SCK-// clock
 	output reg spi_do,				//-SI--// slave in
-	input  reg spi_di					//-DO--// slave out
+	input  spi_di						//-DO--// slave out
+	
 	
 );
 	
+	
 	//Parameter
-	parameter init = 0, decode = 1, wren = 2, read = 3, write = 4;
-	reg [1:0] state, next_state;
+	
+	parameter init = 0, decode = 1, wren = 2, reading = 3, writing = 4;
+	reg [2:0] state, next_state;
+	
 	
 	//Passing
+	
 	always@(posedge clk_50M,negedge reset_n)begin
 		if(!reset_n) state <= 2'b0;
 		else state <= next_state; 
 	end
 	
+	
 	//Transition
+	
+	wire read_done, write_done;
 	always@(*)begin
 		case(state)
 		
 			init     :  next_state = (write == 1'b1) ? decode : init;
 			decode   :  begin
-								case(write_value)begin
-									code_wren  : next_state = wren;
-									code_write : next_state = write;
-									code_read  : next_state = read;
+								case(write_value)
+									`code_wren  : next_state = wren;
+									`code_write : next_state = writing;
+									`code_read  : next_state = reading;
 									
-									default: next_state = idle;
+									default: next_state = init;
 								endcase
 							end
-			wren		:	next_state = (write_complete) ? init : wren; 
-			read     :	next_state = (write_complete) ? init : read;
-			write    :	next_state = (write_complete) ? init : write;
+			wren		:	next_state = (write_complete == 1'b1) ? init : wren; 
+			reading  :	next_state = (read_done == 1'b1) ? init : reading;
+			writing  :	next_state = (write_done == 1'b1) ? init : writing;
 			
 			default  : 	next_state = init;
 			
@@ -86,17 +95,17 @@ module hw2(
 							end
 			wren		:	begin
 								spi_csn  = 1'b0;
-								spi_sck  = ;
-								spi_do   = ;
-								write_complete = ;
+								spi_sck  = wren_sck;
+								spi_do   = wren_spi_do;
+								write_complete = wren_write_complete;
 							end
-			read		:	begin
+			reading	:	begin
 								spi_csn  = 1'b0;
-								spi_sck  = ;
-								spi_do   = ;
-								write_complete = ;
+								spi_sck  = read_sck;
+								spi_do   = read_spi_do;
+								write_complete = read_write_complete;
 							end
-			write		:	begin
+			writing	:	begin
 								spi_csn  = 1'b0;
 								spi_sck  = write_sck;
 								spi_do   = write_spi_do;
@@ -104,29 +113,90 @@ module hw2(
 							end
 			default	:	begin
 								spi_csn  = 1'b1;
-								spi_sck  = ;
-								spi_do   = ;
-								write_complete = ;
+								spi_sck  = 1'b0;
+								spi_do   = 1'b0;
+								write_complete = 1'b0;
 							end
 		endcase
 	end
 	
-	write write_module(
+	//Output Latch
+	
+	wire [7:0] output_buffer;
+	always@(posedge clk_50M,negedge reset_n)begin
+		if(!reset_n) read_value <= 8'd0;
+		else if(read_complete) read_value <= output_buffer;
+		else read_value <= read_value;
+	end
+	
+	//Extrnal Modules
+	
+	wire write_en, send_en, read_en;
+	assign write_en = state == writing;
+	assign send_en = state == wren;
+	assign read_en = state == reading;
+	
+	writer write_module(
 		//System
 		.clk(clk_50M),
 		.reset_n(reset_n),
 		
 		//Controls & Datas
-		.en(state == write),
+		.en(write_en),
 		.write(write),
 		.data(write_value),
 		
 		//Signal out
 		.write_complete(write_write_complete),
 		.sck(write_sck),
-		.spi_do(write_spi_do)
+		.spi_do(write_spi_do),
+		
+		
+		.total_done(write_done)
 	);
 	
+	send_cmd command_send_module(
+		//System
+		.clk(clk_50M),
+		.reset_n(reset_n),
+		
+		//Controls & Datas
+		.en(send_en),
+		.write(write),
+		.data(write_value),
+		
+		//Signal out
+		.write_complete(wren_write_complete),
+		.sck(wren_sck),
+		.spi_do(wren_spi_do),
+		
+	);
+	
+	reader read_module(
+		//System
+		.clk(clk_50M),
+		.reset_n(reset_n),
+		
+		//Controls & Datas
+		.en(read_en),
+		.write(write),
+		.data(write_value),
+		.read(read),
+		
+		//Signal out
+		.write_complete(read_write_complete),
+		.sck(read_sck),
+		.spi_do(read_spi_do),
+		
+		//Retrive data
+		.spi_di(spi_di),
+		.read_data(output_buffer),
+		.read_complete(read_complete),
+		
+		
+		.total_done(read_done)
+		
+	);
 	
 
 endmodule
--- send_cmd.v
+++ send_cmd.v
@@ -5,56 +5,60 @@ module send_cmd(
 	input en,
 	
 	input write,
-	input [7:0] write_data,
+	input [7:0] data,
 	
 	output write_complete,
-	output sck,
-	output spi_do
+	output reg sck,
+	output reg spi_do
 );
 	
 	parameter c0 = 0, c1 = 1, c2 = 2, c3 = 3, c4 = 4, c5 = 5,c6 = 6, c7 = 7;
 	parameter done = 8;
-	reg [2:0] state,next_state;
+	reg [3:0] state, next_state;
 	
 	//SDK Generater
-	reg [4:0] counter;
+	reg [10:0] counter;
 	always@(posedge clk,negedge reset_n)begin
-		if(!reset_n) counter <= 5'h00;
-		else if(counter == sck_counter_max) counter <= 5'h0;
-		else if(!write_complete && en) begin
-			counter <= counter + 5'h1;
-			
-			if(counter < 5'h0f) sck <= 1'b0;
-			else sck <= 1'b1;
-			
+		if(!reset_n) counter <= 11'h00;
+		else if(counter == `sck_counter_max) counter <= 11'h0;
+		else if(en) begin
+			counter <= counter + 11'h1;
 		end
-		else counter <= 5'h0;
+		else counter <= 11'h0;
+	end
+	
+	always@(posedge clk, negedge reset_n)begin
+		if(!reset_n) sck <= 1'b0;
+		else begin
+			if(counter > `sck_counter_half) sck <= 1'b1;
+			else sck <= 1'b0;
+		end
 	end
 	
 	//--FSM--//
 	//Passing
 	always@(posedge clk, negedge reset_n)begin
 		if(!reset_n) state <= 3'd0;
-		else state <= next_state;
+		else if(en) state <= next_state;
+		else state <= 3'd0;
 	end
 	
 	//Transistion
 	always@(*)begin
 		case(state)
 			
-			c0: next_state = (counter == sck_counter_max) ? c1: c0;
-			c1: next_state = (counter == sck_counter_max) ? c2: c1;
-			c2: next_state = (counter == sck_counter_max) ? c3: c2;
-			c3: next_state = (counter == sck_counter_max) ? c4: c3;
-			c4: next_state = (counter == sck_counter_max) ? c5: c4;
-			c5: next_state = (counter == sck_counter_max) ? c6: c5;
-			c6: next_state = (counter == sck_counter_max) ? c7: c6;
-			c7: next_state = (counter == sck_counter_max) ? done: c7;
+			c0: next_state = (counter == `sck_counter_max) ? c1: c0;
+			c1: next_state = (counter == `sck_counter_max) ? c2: c1;
+			c2: next_state = (counter == `sck_counter_max) ? c3: c2;
+			c3: next_state = (counter == `sck_counter_max) ? c4: c3;
+			c4: next_state = (counter == `sck_counter_max) ? c5: c4;
+			c5: next_state = (counter == `sck_counter_max) ? c6: c5;
+			c6: next_state = (counter == `sck_counter_max) ? c7: c6;
+			c7: next_state = (counter == `sck_counter_max) ? done: c7;
 			
 			done: next_state = c0;
 			
 			default: next_state = c0;
-			
 		endcase
 	end
 	
@@ -62,20 +66,22 @@ module send_cmd(
 	always@(*)begin
 		case(state)
 			
-			c0: 
-			c1:
-			c2:
-			c3:
-			c4:
-			c5:
-			c6:
-			c7:
+			c0: spi_do = data[7];
+			c1: spi_do = data[6];
+			c2: spi_do = data[5];
+			c3: spi_do = data[4];
+			c4: spi_do = data[3];
+			c5: spi_do = data[2];
+			c6: spi_do = data[1];
+			c7: spi_do = data[0];
 			
-			done: 
+			done: spi_do = 1'b0;
 			
-			default: 
+			default: spi_do = 1'b0;
 			
 		endcase
 	end
+	
+	assign write_complete = (state == done);
 	
 endmodule 
